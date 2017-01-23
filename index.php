782d5448237968a237bd04f07f081ed356910293
Illia <mor1l@yandex.ua>
2017-01-23 11:53:03 +0200

    added description

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Education</title>
+</head>
+<body>
+    <div>
+        <span>Hello world! This is my home tasks!</span>
+    </div>
+</body>
+</html>
